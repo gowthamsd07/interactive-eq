188b1f4b347a3f43c2ee72a99857684185bce18d
mohamedismail99 <mirsirajudeen@tekion.com>
2024-07-01 20:14:00 +0530

    company page hero video addded

--- src/components/PageComponents/CompanyPage/Hero/Hero.js
+++ src/components/PageComponents/CompanyPage/Hero/Hero.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import CompanyBanner from '../../../../assets/images/company/Companyhero.png';
+import CompanyBannerVideo from '../../../../assets/images/company/company-hero-video.mp4';
 import './Hero.scss';
 
 const HeroPage = () => {
     return (
         <section className="ieq-company-banner bg-surface z-index-2 position-relative">
-            <img
-                className="ieq-company-banner__bg"
-                src={CompanyBanner}
-                alt="company banner background"
-            />
+            <div className="ieq-company-banner__bg">
+                <video className="w-100" autoPlay muted loop={false}>
+                    <source src={CompanyBannerVideo} type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+            </div>
             <div className="ieq-company-banner__content">
                 <h1 className="display-3 text-center text-white">
-                Have you ever asked yourself
+                    Have you ever asked yourself
                 </h1>
                 <p className="h6r text-center text-secondary">
-                “How did this candidate make it so far into the evaluation process?”
+                    “How did this candidate make it so far into the evaluation process?”
                 </p>
             </div>
         </section>
